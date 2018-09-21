0660b06ba4cb3b26a9ae03b65fe38f1aec9f088f
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-09-20 23:43:37 -0500

    probably final version

--- game2.js
+++ game2.js
@@ -1,21 +1,24 @@
 const canvas = document.getElementById('sssnake');
-const canvasWidth = canvasHeight = 400;
-const gridSize = 10;
+const canvasWidth = canvasHeight = 450;
+const gridSize = 15;
 const columnNumber = rowNumber = canvasWidth / gridSize;
 const initial_x = initial_y = gridSize * 5;
 const snake = new Snake(initial_x, initial_y);
 const food = new Food();
 let k, l, n, m, temp1, temp2;
+let score = 0;
+let fps = 15;
+const audio = new Audio('HarryPotterPrologue.mp3');
 
 
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'ArrowRight') {
+    if (event.key === 'ArrowRight' && snake.xSpeed !== -1) {
         snake.direction(1, 0);
-    } else if (event.key === 'ArrowLeft') {
+    } else if (event.key === 'ArrowLeft' && snake.xSpeed !== 1) {
         snake.direction(-1, 0);
-    } else if (event.key === 'ArrowDown') {
+    } else if (event.key === 'ArrowDown' && snake.ySpeed !== -1) {
         snake.direction(0, 1);
-    } else if (event.key === 'ArrowUp') {
+    } else if (event.key === 'ArrowUp' && snake.ySpeed !== 1) {
         snake.direction(0, -1);
     }
 })
@@ -25,6 +28,12 @@ function setCanvas(width, height) {
     canvas.height = height;
     canvas.setAttribute('style', 'background-color: black;')
     ctx = canvas.getContext('2d');
+}
+
+function showScore() {
+    ctx.font = '15px Arial';
+    ctx.fillStyle = 'white';
+    ctx.fillText(score, 15, 15);
 }
 
 function Snake(x, y) {
@@ -74,6 +83,25 @@ function Snake(x, y) {
         this.x += this.xSpeed * gridSize;
         this.y += this.ySpeed * gridSize;
     }
+
+    this.selfHit = function() {
+        let res = false;
+        for (let tail of this.tailArr) {
+            if (this.x === tail[0] && this.y === tail[1]) {
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+
+    this.reset = function() {
+        this.x = initial_x;
+        this.y = initial_y;
+        this.xSpeed = 1;//gridSize;
+        this.ySpeed = 0;//gridSize;
+        this.tailArr = [[this.x-gridSize, this.y], [this.x-2*gridSize, this.y], [this.x-3*gridSize, this.y], [this.x-4*gridSize, this.y]];
+    }
 }
 
 function Food(x, y) {
@@ -83,7 +111,7 @@ function Food(x, y) {
     this.draw = function() {
         ctx.beginPath();
         ctx.rect(this.x, this.y, gridSize - 1, gridSize - 1);
-        ctx.fillStyle = 'white';
+        ctx.fillStyle = '#84FF00';
         ctx.fill();
     }
 
@@ -94,21 +122,27 @@ function Food(x, y) {
 }
 
 
-function timer(func, time) {
-    setInterval(func, time)
-}
-
 function main() {
-    // requestAnimationFrame(main);
+    audio.play();
+    setTimeout(function() {requestAnimationFrame(main)}, 1000 / fps);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    showScore();
     food.draw();
     snake.draw();
     if (food.x === snake.x && food.y === snake.y) {
         food.update();
+        score += 1;
         snake.tailArr.push([0,0]);
+        fps += 1;
     }
     snake.update();
+    if (snake.selfHit()) {
+        snake.reset();
+        score = 0;
+        fps = 15;
+    }
 }
 
 setCanvas(canvasWidth, canvasHeight);
-timer(main, 1000 / 15);
+main();
+// setInterval(main, 1000/fps);
